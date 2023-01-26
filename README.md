0218bb12e1279d1fa8bbe24729c0417daf808219
BorjaM <andres.vorja.vorja@gmail.com>
2023-01-25 21:27:32 -0500

    Project skeleton

---
+++ README.md
@@ -0,0 +1,8 @@
+# I know that word!
+Este proyecto pertenece a:  
+Carlos Andrés Borja - borja.carlos@correounivalle.edu.co     
+Deisy Catalina Melo - deisy.melo@correounivalle.edu.co  
+Universidad del Valle   
+Escuela de Ingeniería de Sistemas y Computación     
+Asignatura: Fundamentos de Programación Orientada a Eventos     
+Profesora: Paola J. Rodríguez C.
---
+++ src/myProject/GUI.java
@@ -0,0 +1,17 @@
+package myProject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
+
+/**
+ * This class is designed in order to view Model class
+ *
+ * @author Deisy Catalina Melo - deisy.melo@correounivalle.edu.co
+ *         Carlos Andrés Borja - borja.carlos@correounivalle.edu.co
+ * @version v.
+ */
+public class GUI extends JFrame {
+}
