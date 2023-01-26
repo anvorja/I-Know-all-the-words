5164855cd9ce83a8a4145f20dcc584174e8f1e63
BorjaM <andres.vorja.vorja@gmail.com>
2023-01-25 22:12:57 -0500

    FileManager was built

--- src/myProject/FileManager.java
+++ src/myProject/FileManager.java
@@ -9,7 +9,7 @@ import java.util.Objects;
  * This class allows to manipulate .txt files to be read or to write to them
  * @author Deisy Catalina Melo - deisy.melo@correounivalle.edu.co
  *         Carlos Andrés Borja - borja.carlos@correounivalle.edu.co
- * @version v.
+ * @version v
  */
 public class FileManager
 {
@@ -19,24 +19,46 @@ public class FileManager
     private BufferedWriter output; // it allows to writes text to a character-output stream
     public static final String bancoDePalabras = "src/myProject/files/bancoDePalabras.txt";   //constant location
     public static final String usuariosListados = "src/myProject/files/usuariosListados.txt"; //constant location
-    public String lecturaFile() {
-        String texto="";
 
-        try {
-            fileReader = new FileReader("src/myProyect/files/diccionario.txt");
+    /**
+     * This method reads a .txt file and returns the arrayList with each word of the file
+     * @param _file
+     * @return ArrayList
+     */
+    public ArrayList<String> leerArchivos(String _file)
+    {
+
+        ArrayList<String> texto = new ArrayList<>();
+
+        String elArchivoLeido = "";
+        if (Objects.equals(_file, "miListaDePalabras"))
+            elArchivoLeido = bancoDePalabras;
+        else if (Objects.equals(_file, "miListaDeUsuarios"))
+            elArchivoLeido = usuariosListados;
+
+        try
+        {
+            //To allows reading of UTF-8. Unicode and ISO 10646 character encoding format
+            fileReader = new FileReader(elArchivoLeido, StandardCharsets.UTF_8);
             input = new BufferedReader(fileReader);
             String line = input.readLine();
-            while(line!=null){
-                texto+=line;
-                texto+="\n";
-                line=input.readLine();
+            while (line != null)
+            {
+                texto.add(line);
+                line = input.readLine();
             }
-        } catch (FileNotFoundException e) {
+        }
+        catch(FileNotFoundException e)
+        {
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch(IOException e)
+        {
             e.printStackTrace();
-        }finally{
-            try {
+        }
+        finally
+        {
+            try
+            {
                 input.close();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -45,21 +67,61 @@ public class FileManager
         return texto;
     }
 
-    public void escribirTexto(String linea){
-        try {
-            fileWriter = new FileWriter("src/myProject/files/fileText.txt",true);
+    /**
+     * This method allows writing to the usuariosListados.txt file
+     * @param linea
+     */
+    public void escribirTexto(String linea)
+    {
+        try
+        {
+            fileWriter = new FileWriter(usuariosListados, true);
             output = new BufferedWriter(fileWriter);
             output.write(linea);
             output.newLine();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
-        }finally{
-            try {
+        } finally
+        {
+            try
+            {
                 output.close();
-            } catch (IOException e) {
+            } catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * This method allows writing to the usuariosListados.txt file in order to update the level
+     * @param posicion
+     * @param nivelNuevo
+     */
+    public void actualizarNivel(int posicion, int nivelNuevo)
+    {
+        try
+        {
+            ArrayList<String> usuariosActualizados = leerArchivos("miListaDeUsuarios");
+            String usuarioAntiguo = usuariosActualizados.get(posicion);
+            String usuarioActualizado = usuarioAntiguo.substring(0, usuarioAntiguo.lastIndexOf(":") + 2) + nivelNuevo;
+            usuariosActualizados.remove(posicion);
+            usuariosActualizados.add(posicion, usuarioActualizado);
+            fileWriter = new FileWriter(usuariosListados, false);
+            output = new BufferedWriter(fileWriter);
+            for (String usuariosActualizado : usuariosActualizados)
+            {
+                output.write(usuariosActualizado);
+                output.newLine();
+
+            }
+            output.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
