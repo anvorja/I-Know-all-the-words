d4b15ae1e0da6b81344919f557edc7df8dc90343
BorjaM <andres.vorja.vorja@gmail.com>
2023-01-25 22:09:43 -0500

    Canvas class

---
+++ src/myProject/Canvas.java
@@ -0,0 +1,56 @@
+package myProject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+/**
+ * @author Deisy Catalina Melo - deisy.melo@correounivalle.edu.co
+ *         Carlos Andrés Borja - borja.carlos@correounivalle.edu.co
+ * @version v
+ */
+
+public class Canvas  extends JPanel
+{
+
+    private int step;
+    private ImageIcon imagenFrame,imagenPanel;
+
+    /**
+     * Constructor of class Canvas
+     * @param option integer to determine which part of the paintComponent method to perform
+     * */
+
+    public Canvas(int option)
+    {
+        step = option;
+    }
+
+    /**
+     * This method is responsible for painting the background of the JFrame and the panelGame
+     * */
+    public void paintComponent(Graphics g)
+    {
+
+        switch (step)
+        {
+            case 1 ->
+            {
+                imagenFrame = new ImageIcon(Objects.requireNonNull(getClass().getResource("/myProject/recursos/inicio.jpg")));
+                imagenFrame = new ImageIcon(imagenFrame.getImage().getScaledInstance(900, 600, Image.SCALE_SMOOTH));
+                g.drawImage(imagenFrame.getImage(), 0, 0, getWidth(), getHeight(), null);
+                setOpaque(false);
+                super.paintComponent(g);
+            }
+            case 2 ->
+            {
+                imagenPanel = new ImageIcon(Objects.requireNonNull(getClass().getResource("/myProject/recursos/fondojuego.jpg")));
+                imagenPanel = new ImageIcon(imagenPanel.getImage().getScaledInstance(700, 375, Image.SCALE_SMOOTH));
+                g.drawImage(imagenPanel.getImage(), 0, 0, getWidth(), getHeight(), null);
+                setOpaque(false);
+                super.paintComponent(g);
+            }
+        }
+
+    }
+}
